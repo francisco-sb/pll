7136be03c44cdafa4cad4e1aaa4b9840423f8b2d
Alfonso Vargas <vc.alfonso@hotmail.com>
2018-03-04 01:58:42 -0600

    Se agrega envio a striper

--- app/Http/Controllers/PllsController.php
+++ app/Http/Controllers/PllsController.php
@@ -25,6 +25,21 @@ class PllsController extends Controller {
         $donor->amount = $request->amount;
         $donor->save();
 
+        \Stripe\Stripe::setApiKey ( 'sk_test_yourSecretkey' );
+        try 
+        {
+            \Stripe\Charge::create (array(
+                    "amount" => $request->amount,
+                    "currency" => "mx", //Verificar la doc
+                    "source" => $request->input ($request->stripeToken), // obtained with Stripe.js
+                    "description" => "Pago del donante".$request->name.' '.$request->lastname
+            ) );
+        } 
+        catch ( \Exception $e ) 
+        {
+		    return view('error');//retorna error de algun pago
+	    }
+
         //retorna la vista de gratitud
         return view('succesfull');
     }
