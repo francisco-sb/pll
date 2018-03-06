00d9a9e96ed9254c8931b95f34d01f19ad5d15e2
Francisco Sierra <fmsb0193@gmail.com>
2018-03-06 14:07:03 -0600

    Error management 1

--- app/Http/Controllers/PllsController.php
+++ app/Http/Controllers/PllsController.php
@@ -48,59 +48,54 @@ class PllsController extends Controller {
             $donor->amount = (float) 0;//$request->amount; --Poner el amount
             $donor->save();
         } catch (Exception $e) {
-
-            //En este se implementará todos los tipos de excepciones y eliminas las otras de arriba
-            switch ($$e->getErrorCode()) {
-                case '1000': //Es de la lista de errores, digo que aqui agreges solo los de las transacciones
-                    $message = 'Ha ocurrido un error en el servidor de OpenPay';
-                    break;
-                case '502':
-                    $message = 'El servicio de pago no se encuentra disponible por el momento, por favor intenta más tarde'
-                    break;
-                case '3001':
-                    $message = 'Tu tarjeta fue declinada'
-                    break;
-                case '3002':
-                    $message = 'Tu tarjeta ha expirado'
-                    break;
-                case '3003':
-                    $message = 'Tu tarjeta no cuenta con los suficientes fondos'
-                    break;
-                case '3004':
-                    $message = 'Procesaste un cargo con una tarjeta reportada como robada'
-                    break;
-                case '3005':
-                    $message = 'Tu tarjeta fue rechazada por el sistema antifraudes'
-                    break;
-                case '3007':
-                    $message = 'Tu tarjeta fue rechazada'
-                    break;
-                case '3008':
-                    $message = 'Tu tarjeta no soporta transacciones online'
-                    break;
-                case '3009':
-                    $message = 'Procesaste un cargo con una tarjeta reportada como perdida'
-                    break;
-                case '3010':
-                    $message = 'Tarjeta bloqueada por el banco'
-                    break;
-                case '3002':
-                    $message = 'Tarjeta declinada'
-                    break;
-                case '3012':
-                    $message = 'Se requiere autorización del banco'
-                    break;
-                default:
-                    $message = 'Lo sentimos, ocurrió un error con tu transacción...'
-                    break;
-            }
-
-            return view('index', ['status' => false,
+            $message = 'Código de Error: ' . $e->getErrorCode() . ' - ' . $e->getDescription();
+            // switch ($error_code) {
+            //     case 1000: //Es de la lista de errores, digo que aqui agreges solo los de las transacciones
+            //         $message = 'Ha ocurrido un error en el servidor de OpenPay';
+            //         break;
+            //     case 502:
+            //         $message = 'El servicio de pago no se encuentra disponible por el momento, por favor intenta más tarde';
+            //         break;
+            //     case 3001:
+            //         $message = 'Tu tarjeta fue declinada';
+            //         break;
+            //     case 3002:
+            //         $message = 'Tu tarjeta ha expirado';
+            //         break;
+            //     case 3003:
+            //         $message = 'Tu tarjeta no cuenta con los suficientes fondos';
+            //         break;
+            //     case 3004:
+            //         $message = 'Procesaste un cargo con una tarjeta reportada como robada';
+            //         break;
+            //     case 3005:
+            //         $message = 'Tu tarjeta fue rechazada por el sistema antifraudes';
+            //         break;
+            //     case 3007:
+            //         $message = 'Tu tarjeta fue rechazada';
+            //         break;
+            //     case 3008:
+            //         $message = 'Tu tarjeta no soporta transacciones online';
+            //         break;
+            //     case 3009:
+            //         $message = 'Procesaste un cargo con una tarjeta reportada como perdida';
+            //         break;
+            //     case 3010:
+            //         $message = 'Tarjeta bloqueada por el banco';
+            //         break;
+            //     case 3012:
+            //         $message = 'Se requiere autorización del banco';
+            //         break;
+            //     default:
+            //         $message = 'Lo sentimos, ocurrió un error con tu transacción...';
+            // }
+            //fail
+            return view('fail', ['status' => 2,
                                   'message' => $message]);
         }
 
-        //retorna la vista de gratitud
-        return view('index', ['status' => true,
+        //success
+        return view('success', ['status' => 1,
                               'message' => '¡Gracias por tu donativo!']);
     }
 
