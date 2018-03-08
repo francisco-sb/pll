f4e7970da874a02247cf4b607365eef7a0cc4e3b
Francisco Sierra <fmsb0193@gmail.com>
2018-03-07 22:01:18 -0600

    changes in PllController and OpenPayValidator

--- app/Http/Controllers/PllsController.php
+++ app/Http/Controllers/PllsController.php
@@ -26,77 +26,80 @@ class PllsController extends Controller {
             $openValidator = new OpenPayValidator;
             $validator = $openValidator->RequestValidator($request);
 
-            if ($validator->passes()) 
+            if ($validator->passes())
             {
-                // try
-                // {
-                //     Openpay::setProductionMode(false);
-                //     $openpay = Openpay::getInstance('mnfv5dmxgwgvxdjstxng', 'sk_70f2c7afaa854d4bb9b160653fd4c263');
-                //
-                //     $customer = array(
-                //         'name' => $request->name,
-                //         'last_name' => $request->lastname,
-                //         'email' => $request->email);
-                //
-                //     $chargeData = array(
-                //         'method' => 'card',
-                //         'source_id' => $request->token_id,
-                //         // 'amount' => (float)$request->amount, --Poner el amount
-                //         'amount' => (float)100,
-                //         'description' => "Donación por la persona: ".$request->name,
-                //         'device_session_id' => $request->deviceIdHiddenFieldName,
-                //         'customer' => $customer
-                //         );
-                //
-                //     $charge = $openpay->charges->create($chargeData);
-                //
-                //     //Si no existe error, guardamos el donante.
-                //     $donor = new Donor;
-                //     $donor->name = $request->name;
-                //     $donor->lastname = $request->lastname;
-                //     $donor->email = $request->email;
-                //     $donor->amount = (float) 0;//$request->amount; --Poner el amount
-                //     $donor->save();
-                //
-                // } catch (OpenpayApiTransactionError $e) {
-                //   	$message = 'ERROR en la transacción: ' . $e->getMessage() .
-                //   	      ' [error code: ' . $e->getErrorCode() .
-                //   	      ', error category: ' . $e->getCategory() .
-                //   	      ', HTTP code: '. $e->getHttpCode() .
-                //   	      ', request ID: ' . $e->getRequestId() . ']';
-                //
-                //           return view('fail', ['status' => 2,
-                //                                 'message' => $message]);
-                // } catch (OpenpayApiRequestError $e) {
-                // 	  $message = 'ERROR en la solicitud: ' . $e->getMessage();
-                //     return view('fail', ['status' => 2,
-                //                           'message' => $message]);
-                // } catch (OpenpayApiConnectionError $e) {
-                // 	  $message = 'ERROR al conectarse con la API de Openpay: ' . $e->getMessage();
-                //     return view('fail', ['status' => 2,
-                //                           'message' => $message]);
-                // } catch (OpenpayApiAuthError $e) {
-                // 	  $message = 'ERROR de autenticación: ' . $e->getMessage();
-                //     return view('fail', ['status' => 2,
-                //                           'message' => $message]);
-                // } catch (OpenpayApiError $e) {
-                // 	  $message = 'ERROR en la API: ' . $e->getMessage();
-                //     return view('fail', ['status' => 2,
-                //                           'message' => $message]);
-                // } catch (Exception $e) {
-                //   	$message = 'Error en el script: ' . $e->getMessage();
-                //     //fail
-                //     return view('fail', ['status' => 2,
-                //                           'message' => $message]);
-                // }
-                // //success
-                // return view('success', ['status' => 1,
-                //                       'message' => '¡Gracias por tu donativo!']);
+                try
+                {
+                    Openpay::setProductionMode(false);
+                    $openpay = Openpay::getInstance('mnfv5dmxgwgvxdjstxng', 'sk_70f2c7afaa854d4bb9b160653fd4c263');
+
+                    $customer = array(
+                        'name' => $request->name,
+                        'last_name' => $request->lastname,
+                        'email' => $request->email);
+
+                    //$customer = $openpay->customers->add($customerData);
+
+                    $chargeData = array(
+                        'method' => 'card',
+                        'source_id' => $request->token_id,
+                        // 'amount' => (float)$request->amount, --Poner el amount
+                        'amount' => (float)100,
+                        'description' => "Donación por la persona: ".$request->name,
+                        'device_session_id' => $request->deviceIdHiddenFieldName,
+                        'customer' => $customer
+                        );
+
+                    $charge = $openpay->charges->create($chargeData);
+
+                    // //Si no existe error, guardamos el donante.
+                    // $donor = new Donor;
+                    // $donor->name = $request->name;
+                    // $donor->lastname = $request->lastname;
+                    // $donor->email = $request->email;
+                    // $donor->amount = (float) 0;//$request->amount; --Poner el amount
+                    // $donor->save();
+
+                } catch (OpenpayApiTransactionError $e) {
+                  	$message = 'ERROR en la transacción: ' . $e->getMessage() .
+                  	      ' [error code: ' . $e->getErrorCode() .
+                  	      ', error category: ' . $e->getCategory() .
+                  	      ', HTTP code: '. $e->getHttpCode() .
+                  	      ', request ID: ' . $e->getRequestId() . ']';
+                    //return response()->json(['error' => $message]);
+                    error_log($message);
+
+                } catch (OpenpayApiRequestError $e) {
+                	  $message = 'ERROR en la solicitud: ' . $e->getMessage();
+                    return response()->json(['error' => $message]);
+                    error_log($message);
+
+                } catch (OpenpayApiConnectionError $e) {
+                	  $message = 'ERROR al conectarse con la API de Openpay: ' . $e->getMessage();
+                    return response()->json(['error' => $message]);
+                    error_log($message);
+
+                } catch (OpenpayApiAuthError $e) {
+                	  $message = 'ERROR de autenticación: ' . $e->getMessage();
+                    return response()->json(['error' => $message]);
+                    error_log($message);
+
+                } catch (OpenpayApiError $e) {
+                	  $message = 'ERROR en la API: ' . $e->getMessage();
+                    return response()->json(['error' => $message]);
+                    error_log($message);
+
+                } catch (Exception $e) {
+                  	$message = 'Error en el script: ' . $e->getMessage();
+                    return response()->json(['error' => $message]);
+                    error_log($message);
+
+                }
 
                 return response()->json(['success' => "¡Gracias por tu donativo!"]);
             }
 
-            return response()->json(['errors' => $validator->errors()]);
+            return response()->json(['errors_form' => $validator->errors()]);
         }
     }
 
@@ -110,6 +113,6 @@ class PllsController extends Controller {
         return "Eliminar donante";
     }
 
-    
+
 
 }
--- app/Http/Validators/OpenPayValidator.php
+++ app/Http/Validators/OpenPayValidator.php
@@ -5,14 +5,28 @@ use Illuminate\Support\Facades\Validator;
 
 class OpenPayValidator
 {
-    public function RequestValidator(Request $request) 
+    public function RequestValidator(Request $request)
     {
         $rules = [
             'name' => 'required',
+            'lastname' => 'required',
+            'email' => 'required',
+            'holder_name' => 'required',
+            'cvv2' => 'required',
+            'card_number' => 'required',
+            'expiration_month' => 'required',
+            'expiration_year' => 'required',
         ];
 
         $message = [
-            'name.required' => 'El nombre es requerido',
+            'name.required' => 'Nombre es requerido',
+            'lastname.required' => 'Apellido es requerido',
+            'email.required' => 'Correo electrónico es requerido',
+            'holder_name.required' => 'Nombre del titular es requerido',
+            'cvv2.required' => 'Código de seguridad es requerido',
+            'card_number.required' => 'Número de tarjeta es requerido',
+            'expiration_month.required' => 'Mes es requerido',
+            'expiration_year.required' => 'Año es requerido',
         ];
 
         $validator = Validator::make($request->all(), $rules, $message);
