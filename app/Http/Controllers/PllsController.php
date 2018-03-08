c252569c8f95bc7af5a9c7dd30a5bd48127de1dc
Francisco Sierra <fmsb0193@gmail.com>
2018-03-08 04:48:06 -0600

    Saving the amount

--- app/Http/Controllers/PllsController.php
+++ app/Http/Controllers/PllsController.php
@@ -33,42 +33,30 @@ class PllsController extends Controller {
                     Openpay::setProductionMode(false);
                     $openpay = Openpay::getInstance('mnfv5dmxgwgvxdjstxng', 'sk_70f2c7afaa854d4bb9b160653fd4c263');
 
-                    $webhook = array('url' => '/pll',
-                        'user' => '',
-                        'password' => 'Porlalibre1.',
-                        'event_types' => array(
-                            'charge.failed',
-                            'charge.created'
-                          )
-                    );
-
                     $customer = array(
                         'name' => $request->name,
                         'last_name' => $request->lastname,
                         'email' => $request->email);
 
-                    //$customer = $openpay->customers->add($customerData);
-
                     $chargeData = array(
                         'method' => 'card',
                         'source_id' => $request->token_id,
-                        // 'amount' => (float)$request->amount, --Poner el amount
-                        'amount' => (float)100,
+                        'amount' => (float)$request->amount,
                         'description' => "Donación por la persona: ".$request->name,
                         'device_session_id' => $request->deviceIdHiddenFieldName,
                         'customer' => $customer
                         );
 
                     $charge = $openpay->charges->create($chargeData);
-                    $webhook = $openpay->webhooks->create($webhook);
 
-                    //Si no existe error, guardamos el donante.
+                    // //Si no existe error, guardamos el donante.
                     $donor = new Donor;
                     $donor->name = $request->name;
                     $donor->lastname = $request->lastname;
                     $donor->email = $request->email;
-                    $donor->amount = (float) 0;//$request->amount; --Poner el amount
+                    $donor->amount = (float)$request->amount;
                     $donor->save();
+
                 } catch (\OpenpayApiTransactionError $e) {
                   	$message = 'ERROR en la transacción: ' . $e->getMessage() .
                   	      ' [error code: ' . $e->getErrorCode() .
@@ -80,18 +68,23 @@ class PllsController extends Controller {
                 } catch (\OpenpayApiRequestError $e) {
                 	  $message = 'ERROR en la solicitud: ' . $e->getMessage();
                     return response()->json(['error' => $message]);
+
                 } catch (\OpenpayApiConnectionError $e) {
                 	  $message = 'ERROR al conectarse con la API de Openpay: ' . $e->getMessage();
                     return response()->json(['error' => $message]);
+
                 } catch (\OpenpayApiAuthError $e) {
                 	  $message = 'ERROR de autenticación: ' . $e->getMessage();
                     return response()->json(['error' => $message]);
+
                 } catch (\OpenpayApiError $e) {
                 	  $message = 'ERROR en la API: ' . $e->getMessage();
                     return response()->json(['error' => $message]);
+
                 } catch (\Exception $e) {
                   	$message = 'Error en el script: ' . $e->getMessage();
                     return response()->json(['error' => $message]);
+
                 }
 
                 return response()->json(['success' => "¡Gracias por tu donativo!"]);
