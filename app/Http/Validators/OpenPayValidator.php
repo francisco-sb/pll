885a601b8f25d58b7bce5d723dc5e140ea4d0d3b
Francisco Sierra <fmsb0193@gmail.com>
2018-03-08 04:47:37 -0600

    Validation amount

--- app/Http/Validators/OpenPayValidator.php
+++ app/Http/Validators/OpenPayValidator.php
@@ -16,17 +16,19 @@ class OpenPayValidator
             'card_number' => 'required',
             'expiration_month' => 'required',
             'expiration_year' => 'required',
+            'amount' => 'required',
         ];
 
         $message = [
-            'name.required' => 'Nombre es requerido',
-            'lastname.required' => 'Apellido es requerido',
-            'email.required' => 'Correo electrónico es requerido',
-            'holder_name.required' => 'Nombre del titular es requerido',
-            'cvv2.required' => 'Código de seguridad es requerido',
-            'card_number.required' => 'Número de tarjeta es requerido',
-            'expiration_month.required' => 'Mes es requerido',
-            'expiration_year.required' => 'Año es requerido',
+            'name.required' => 'Nombre requerido',
+            'lastname.required' => 'Apellido requerido',
+            'email.required' => 'Correo electrónico requerido',
+            'holder_name.required' => 'Nombre del titular requerido',
+            'cvv2.required' => 'Código de seguridad requerido',
+            'card_number.required' => 'Número de tarjeta requerido',
+            'expiration_month.required' => 'Mes requerido',
+            'expiration_year.required' => 'Año requerido',
+            'amount.required' => 'Cantidad requerida'
         ];
 
         $validator = Validator::make($request->all(), $rules, $message);
